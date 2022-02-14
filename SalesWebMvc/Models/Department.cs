ea531edc4f4800730ff8a6e8a61168b96099e6c9
yagocrispim0 <yagocrispimgomes@gmail.com>
2022-02-14 00:12:08 -0300

    Added SeedingService, but not implemented

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -4,7 +4,7 @@ namespace SalesWebMvc.Models
 {
     public class Department
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set;} = new List<Seller>();
 
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 
 
@@ -9,6 +10,7 @@ string connectionString = builder.Configuration.GetConnectionString("SalesWebMvc
 
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), builder => builder.MigrationsAssembly("SalesWebMvc")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -21,6 +23,18 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    using (var scope = app.Services.CreateScope())
+    {
+        /*var dbContext = scope.ServiceProvider.GetRequiredService<SalesWebMvcContext>();
+        // use context
+        SeedingService ss = new SeedingService(dbContext);
+        ss.Seed();*/
+    }
+
 }
 
 app.UseHttpsRedirection();
@@ -35,3 +49,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
